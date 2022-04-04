e6b622e95a5fee198300ada1f8cd2408fa885801
Artur Allmann <Artur.Allmann@tptlive.ee>
2022-04-04 09:14:36 +0300

    sum.test.js requires jest

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,6 @@
-const sum = require('./sum');
+const test = require("jest");
+const sum = require("./sum");
 
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
